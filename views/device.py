475db663e4f7ef46ea9579cd1efbde3e9ab830d9
lin <dijkstralin@qq.com>
2025-07-13 21:41:05 +0800

    设备sdk

--- views/device.py
+++ views/device.py
@@ -33,3 +33,77 @@ def set(box_no):
     request_data = json.loads(request.get_data())
     return jsonify(device_set(current_app, box_no, request_data["name"], request_data["value"]))
 
+import json
+from tencentcloud.common import credential
+from tencentcloud.common.profile.client_profile import ClientProfile
+from tencentcloud.common.profile.http_profile import HttpProfile
+
+
+# from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
+# try:
+#     print("enter")
+# except TencentCloudSDKException as err:
+#     print(err)
+
+cred = credential.Credential()
+httpProfile = HttpProfile()
+httpProfile.endpoint = "iotvideo.tencentcloudapi.com"
+clientProfile = ClientProfile()
+clientProfile.httpProfile = httpProfile
+
+# 上下线记录
+from tencentcloud.iotvideo.v20211125 import iotvideo_client, models
+client = iotvideo_client.IotvideoClient(cred, "ap-guangzhou", clientProfile)
+
+req = models.DescribeDeviceStatusLogRequest()
+params = {
+    "MinTime": 1752163200000,
+    "MaxTime": 1752422399999,
+    "ProductId": "7OWJE12PRR",
+    "DeviceName": "338222062369",
+    "Limit": 10
+}
+req.from_json_string(json.dumps(params))
+resp = client.DescribeDeviceStatusLog(req)
+print(resp.to_json_string())
+
+
+# 实时信息
+from tencentcloud.iotexplorer.v20190423 import iotexplorer_client, models
+httpProfile = HttpProfile()
+httpProfile.endpoint = "iotexplorer.tencentcloudapi.com"
+clientProfile = ClientProfile()
+clientProfile.httpProfile = httpProfile
+client = iotexplorer_client.IotexplorerClient(cred, "ap-guangzhou", clientProfile)
+req = models.DescribeDeviceDataRequest()
+params = {
+    "ProductId": "7OWJE12PRR",
+    "DeviceName": "338222062369"
+}
+req.from_json_string(json.dumps(params))
+resp = client.DescribeDeviceData(req)
+print(resp.to_json_string())
+
+
+############
+
+# 历史信息
+from tencentcloud.iotexplorer.v20190423 import iotexplorer_client, models
+httpProfile = HttpProfile()
+httpProfile.endpoint = "iotexplorer.tencentcloudapi.com"
+clientProfile = ClientProfile()
+clientProfile.httpProfile = httpProfile
+client = iotexplorer_client.IotexplorerClient(cred, "ap-guangzhou", clientProfile)
+req = models.DescribeDeviceDataHistoryRequest()
+params = {
+    "MinTime": 1752404870000,
+    "MaxTime": 1752404876852,
+    "ProductId": "7OWJE12PRR",
+    "DeviceName": "338222062369",
+    "FieldName": "pm2_5",
+    "Limit": 10
+}
+req.from_json_string(json.dumps(params))
+resp = client.DescribeDeviceDataHistory(req)
+print(resp.to_json_string())
+
